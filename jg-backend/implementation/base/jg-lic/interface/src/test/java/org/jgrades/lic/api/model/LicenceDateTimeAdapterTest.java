fa912492ac5d369ccf619a9a552f2c27d9dd7638
baart92 <baart92@gmail.com>
2015-07-18 09:36:04 +0200

    More tests for licence DateTIme formatter

---
+++ jg-backend/implementation/base/jg-lic/interface/src/test/java/org/jgrades/lic/api/model/LicenceDateTimeAdapterTest.java
@@ -0,0 +1,38 @@
+package org.jgrades.lic.api.model;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LicenceDateTimeAdapterTest {
+    @Test
+    public void shouldReturnCorrectFormatter() throws Exception {
+        // given
+        DateTime dateTime = new DateTime(2015, 7, 19, 5, 59, 59);
+        String expectedString = "2015-07-19 5:59:59";
+
+        // when
+        DateTimeFormatter formatter = LicenceDateTimeAdapter.getLicDateTimeFormatter();
+        String dateTimeString = dateTime.toString(formatter);
+
+        // then
+        assertThat(dateTimeString).isEqualTo(expectedString);
+    }
+
+    @Test
+    public void shouldParseStringInCorrectFormat() throws Exception {
+        // given
+        String inputDateTimeString = "2015-07-19 05:59:59";
+        DateTime expectedDateTime = new DateTime(2015, 7, 19, 5, 59, 59);
+
+        // when
+        DateTimeFormatter formatter = LicenceDateTimeAdapter.getLicDateTimeFormatter();
+        DateTime dateTime = DateTime.parse(inputDateTimeString, formatter);
+
+        // then
+        assertThat(dateTime).isEqualTo(expectedDateTime);
+    }
+}
