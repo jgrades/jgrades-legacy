bff34b1e107704d79d7970b7e2ad121aa4c1067b
baart92 <baart92@gmail.com>
2015-07-27 23:43:27 +0200

    Refactoring of JGLoggingFactory and JGradesLogger + fix issues

--- jg-logging/src/main/java/org/jgrades/logging/logger/JGLoggingFactory.java
+++ jg-logging/src/main/java/org/jgrades/logging/logger/JGLoggingFactory.java
@@ -1,24 +1,11 @@
 package org.jgrades.logging.logger;
 
-import org.apache.commons.lang3.StringUtils;
-import org.jgrades.logging.logger.configuration.LoggingConfiguration;
-import org.jgrades.logging.logger.utils.PropertyUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-import sun.rmi.runtime.Log;
+public final class JGLoggingFactory {
+    private JGLoggingFactory() {
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import static org.jgrades.logging.logger.configuration.LoggingConfiguration.LOG_PER_TYPE;
-
-public class JGLoggingFactory {
+    }
 
     public static JGradesLogger getLogger(Class clazz) {
         return new JGradesLogger(clazz);
     }
-
 }
--- jg-logging/src/main/java/org/jgrades/logging/logger/JGradesLogger.java
+++ jg-logging/src/main/java/org/jgrades/logging/logger/JGradesLogger.java
@@ -1,247 +1,296 @@
 package org.jgrades.logging.logger;
 
+import ch.qos.logback.classic.Level;
 import org.apache.commons.lang3.StringUtils;
-import org.jgrades.logging.logger.configuration.LoggingConfiguration;
 import org.jgrades.logging.logger.utils.PropertyUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.slf4j.Marker;
 
-import java.io.IOException;
-import java.util.logging.Level;
-
-
 public class JGradesLogger {
+    private static final String MDC_KEY = "logging_configuration";
 
-    private final String LOGGER_NAME = "logger";
-    private final Logger logger = LoggerFactory.getLogger(LOGGER_NAME);
+    private final Logger logger;
+    private final String moduleName;
 
-    public static JGradesLogger jGradesLogger;
-    private static String moduleName;
-
-    public JGradesLogger(Class clazz){
-        this.moduleName = StringUtils.split(clazz.getName(), ".")[2];
-    }
-
-    public void info(String format) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(format);
-    }
-
-    public void info(String format, Object arg)  {
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(format, arg);
-    }
-
-    public void info(String format, Object arg1, Object arg2) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(format,arg1,arg2);
-    }
-
-    public void info(String format, Object[] argArray) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(format,argArray);
-    }
-
-    public void info(String msg, Throwable t) {
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(msg, t);
-    }
-
-    public void info(Marker marker, String msg) {
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(marker,msg);
-    }
-
-    public void info(Marker marker, String format, Object arg) {
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(marker,format,arg);
-    }
-
-    public void info(Marker marker, String format, Object arg1, Object arg2) {
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(marker,format,arg1,arg2);
-    }
-
-    public void info(Marker marker, String format, Object[] argArray) {
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(marker,format, argArray);
-    }
-
-    public void info(Marker marker, String msg, Throwable t) {
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.info(marker, msg, t);
-
-    }
-
-    //------------------------------------ ERROR ----------------------------------//
-    public void error(String msg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(msg);
-    }
-
-    public void error(String format, Object arg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(format, arg);
-    }
-
-    public void error(String format, Object arg1, Object arg2) {
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(format, arg1, arg2);
-    }
-
-    public void error(String format, Object[] argArray) {
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(format, argArray);
-    }
-
-    public void error(String msg, Throwable t){
-        setMDC(Level.INFO, PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(msg, t);
-    }
-
-    public void error(Marker marker, String msg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(marker, msg);
-    }
-
-    public void error(Marker marker, String format, Object arg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(marker, format, arg);
-    }
-
-    public void error(Marker marker, String format, Object arg1, Object arg2) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(marker, format, arg1, arg2);
-    }
-
-    public void error(Marker marker, String format, Object[] argArray){
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(marker, format, argArray);
-    }
-
-    public void error(Marker marker, String msg, Throwable t) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.error(marker, msg, t);
+    public JGradesLogger(Class clazz) {
+        this.logger = LoggerFactory.getLogger(clazz);
+        this.moduleName = getModuleName(clazz);
     }
 
     //------------------------------------ TRACE ----------------------------------//
-    public void trace(String msg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(msg);
+    public void trace(String paramString) {
+        setMDC(Level.TRACE);
+        logger.trace(paramString);
     }
 
-    public void trace(String format, Object arg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(format, arg);
+    public void trace(String paramString, Object paramObject) {
+        setMDC(Level.TRACE);
+        logger.trace(paramString, paramObject);
     }
 
-    public void trace(String format, Object arg1, Object arg2) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(format, arg1, arg2);
+    public void trace(String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.TRACE);
+        logger.trace(paramString, paramObject1, paramObject2);
     }
 
-    public void trace(String format, Object[] argArray) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(format, argArray);
+    public void trace(String paramString, Object... paramVarArgs) {
+        setMDC(Level.TRACE);
+        logger.trace(paramString, paramVarArgs);
     }
 
-    public void trace(String msg, Throwable t) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(msg, t);
+    public void trace(String paramString, Throwable paramThrowable) {
+        setMDC(Level.TRACE);
+        logger.trace(paramString, paramThrowable);
     }
 
-    public void trace(Marker marker, String msg){
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(marker, msg);
+    public void trace(Marker marker, String paramString) {
+        setMDC(Level.TRACE);
+        logger.trace(marker, paramString);
     }
 
-    public void trace(Marker marker, String format, Object arg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(marker, format, arg);
+    public void trace(Marker marker, String paramString, Object paramObject) {
+        setMDC(Level.TRACE);
+        logger.trace(marker, paramString, paramObject);
     }
 
-    public void trace(Marker marker, String format, Object arg1, Object arg2){
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(marker, format, arg1, arg2);
+    public void trace(Marker marker, String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.TRACE);
+        logger.trace(marker, paramString, paramObject1, paramObject2);
     }
 
-    public void trace(Marker marker, String format, Object[] argArray) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(marker, format, argArray);
+    public void trace(Marker marker, String paramString, Object[] paramVarArgs) {
+        setMDC(Level.TRACE);
+        logger.trace(marker, paramString, paramVarArgs);
     }
 
-    public void trace(Marker marker, String msg, Throwable t) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.trace(marker, msg, t);
+    public void trace(Marker marker, String paramString, Throwable paramThrowable) {
+        setMDC(Level.TRACE);
+        logger.trace(marker, paramString, paramThrowable);
     }
 
     //------------------------------------ DEBUG ----------------------------------//
-
-    public void debug(String msg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(msg);
-    }
-    public void debug(String format, Object arg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(format,arg);
+    public void debug(String paramString) {
+        setMDC(Level.DEBUG);
+        logger.debug(paramString);
     }
 
-    public void debug(String format, Object arg1, Object arg2) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(format,arg1,arg2);
+    public void debug(String paramString, Object paramObject) {
+        setMDC(Level.DEBUG);
+        logger.debug(paramString, paramObject);
     }
 
-    public void debug(String format, Object[] argArray) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(format,argArray);
+    public void debug(String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.DEBUG);
+        logger.debug(paramString, paramObject1, paramObject2);
     }
 
-    public void debug(String msg, Throwable t) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(msg,t);
+    public void debug(String paramString, Object... paramVarArgs) {
+        setMDC(Level.DEBUG);
+        logger.debug(paramString, paramVarArgs);
     }
 
-    public void debug(Marker marker, String msg)  {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(marker,msg);
+    public void debug(String paramString, Throwable paramThrowable) {
+        setMDC(Level.DEBUG);
+        logger.debug(paramString, paramThrowable);
     }
 
-    public void debug(Marker marker, String format, Object arg) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(marker,format,arg);
+    public void debug(Marker marker, String paramString) {
+        setMDC(Level.DEBUG);
+        logger.debug(marker, paramString);
     }
 
-    public void debug(Marker marker, String format, Object arg1, Object arg2) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(marker,format,arg1,arg2);
+    public void debug(Marker marker, String paramString, Object paramObject) {
+        setMDC(Level.DEBUG);
+        logger.debug(marker, paramString, paramObject);
     }
 
-    public void debug(Marker marker, String format, Object[] argArray){
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(marker,format,argArray);
+    public void debug(Marker marker, String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.DEBUG);
+        logger.debug(marker, paramString, paramObject1, paramObject2);
     }
 
-    public void debug(Marker marker, String msg, Throwable t) {
-        setMDC(Level.INFO,PropertyUtils.getCurrentLoggerConfiguration());
-        logger.debug(marker,msg,t);
+    public void debug(Marker marker, String paramString, Object[] paramVarArgs) {
+        setMDC(Level.DEBUG);
+        logger.debug(marker, paramString, paramVarArgs);
     }
 
+    public void debug(Marker marker, String paramString, Throwable paramThrowable) {
+        setMDC(Level.DEBUG);
+        logger.debug(marker, paramString, paramThrowable);
+    }
 
-    private static void setMDC(Level loggingLevel, LoggingConfiguration currenctConfiguration) {
-        switch(currenctConfiguration) {
+    //------------------------------------ INFO ----------------------------------//
+    public void info(String paramString) {
+        setMDC(Level.INFO);
+        logger.info(paramString);
+    }
+
+    public void info(String paramString, Object paramObject) {
+        setMDC(Level.INFO);
+        logger.info(paramString, paramObject);
+    }
+
+    public void info(String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.INFO);
+        logger.info(paramString, paramObject1, paramObject2);
+    }
+
+    public void info(String paramString, Object... paramVarArgs) {
+        setMDC(Level.INFO);
+        logger.info(paramString, paramVarArgs);
+    }
+
+    public void info(String paramString, Throwable paramThrowable) {
+        setMDC(Level.INFO);
+        logger.info(paramString, paramThrowable);
+    }
+
+    public void info(Marker marker, String paramString) {
+        setMDC(Level.INFO);
+        logger.info(marker, paramString);
+    }
+
+    public void info(Marker marker, String paramString, Object paramObject) {
+        setMDC(Level.INFO);
+        logger.info(marker, paramString, paramObject);
+    }
+
+    public void info(Marker marker, String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.INFO);
+        logger.info(marker, paramString, paramObject1, paramObject2);
+    }
+
+    public void info(Marker marker, String paramString, Object... paramVarArgs) {
+        setMDC(Level.INFO);
+        logger.info(marker, paramString, paramVarArgs);
+    }
+
+    public void info(Marker marker, String paramString, Throwable paramThrowable) {
+        setMDC(Level.INFO);
+        logger.info(marker, paramString, paramThrowable);
+
+    }
+
+    //------------------------------------ WARN ----------------------------------//
+    public void warn(String paramString) {
+        setMDC(Level.WARN);
+        logger.warn(paramString);
+    }
+
+    public void warn(String paramString, Object paramObject) {
+        setMDC(Level.WARN);
+        logger.warn(paramString, paramObject);
+    }
+
+    public void warn(String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.WARN);
+        logger.warn(paramString, paramObject1, paramObject2);
+    }
+
+    public void warn(String paramString, Object... paramVarArgs) {
+        setMDC(Level.WARN);
+        logger.warn(paramString, paramVarArgs);
+    }
+
+    public void warn(String paramString, Throwable paramThrowable) {
+        setMDC(Level.WARN);
+        logger.warn(paramString, paramThrowable);
+    }
+
+    public void warn(Marker marker, String paramString) {
+        setMDC(Level.WARN);
+        logger.warn(marker, paramString);
+    }
+
+    public void warn(Marker marker, String paramString, Object paramObject) {
+        setMDC(Level.WARN);
+        logger.warn(marker, paramString, paramObject);
+    }
+
+    public void warn(Marker marker, String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.WARN);
+        logger.warn(marker, paramString, paramObject1, paramObject2);
+    }
+
+    public void warn(Marker marker, String paramString, Object... paramVarArgs) {
+        setMDC(Level.WARN);
+        logger.warn(marker, paramString, paramVarArgs);
+    }
+
+    public void warn(Marker marker, String paramString, Throwable paramThrowable) {
+        setMDC(Level.WARN);
+        logger.warn(marker, paramString, paramThrowable);
+    }
+
+    //------------------------------------ ERROR ----------------------------------//
+    public void error(String paramString) {
+        setMDC(Level.ERROR);
+        logger.error(paramString);
+    }
+
+    public void error(String paramString, Object paramObject) {
+        setMDC(Level.ERROR);
+        logger.error(paramString, paramObject);
+    }
+
+    public void error(String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.ERROR);
+        logger.error(paramString, paramObject1, paramObject2);
+    }
+
+    public void error(String paramString, Object... paramVarArgs) {
+        setMDC(Level.ERROR);
+        logger.error(paramString, paramVarArgs);
+    }
+
+    public void error(String paramString, Throwable paramThrowable) {
+        setMDC(Level.ERROR);
+        logger.error(paramString, paramThrowable);
+    }
+
+    public void error(Marker marker, String paramString) {
+        setMDC(Level.ERROR);
+        logger.error(marker, paramString);
+    }
+
+    public void error(Marker marker, String paramString, Object paramObject) {
+        setMDC(Level.ERROR);
+        logger.error(marker, paramString, paramObject);
+    }
+
+    public void error(Marker marker, String paramString, Object paramObject1, Object paramObject2) {
+        setMDC(Level.ERROR);
+        logger.error(marker, paramString, paramObject1, paramObject2);
+    }
+
+    public void error(Marker marker, String paramString, Object... paramVarArgs) {
+        setMDC(Level.ERROR);
+        logger.error(marker, paramString, paramVarArgs);
+    }
+
+    public void error(Marker marker, String paramString, Throwable paramThrowable) {
+        setMDC(Level.ERROR);
+        logger.error(marker, paramString, paramThrowable);
+    }
+
+    private String getModuleName(Class clazz) {
+        return StringUtils.split(clazz.getName(), ".")[2];
+    }
+
+    private void setMDC(Level loggingLevel) {
+        switch (PropertyUtils.getCurrentLoggerConfiguration()) {
             case LOG_PER_TYPE:
                 MDC.put(StringUtils.EMPTY, StringUtils.EMPTY);
                 break;
             case LOG_PER_MODULE:
-                MDC.put("logging_configuration",moduleName);
+                MDC.put(MDC_KEY, moduleName);
                 break;
             case LOG_PER_TYPE_MODULE:
-                MDC.put("logging_configuration",moduleName+"_"+loggingLevel.toString().toLowerCase());
+                String moduleNameWithLoggingLevel = moduleName + "_" + loggingLevel.toString().toLowerCase();
+                MDC.put(MDC_KEY, moduleNameWithLoggingLevel);
                 break;
         }
     }
-
-
 }
