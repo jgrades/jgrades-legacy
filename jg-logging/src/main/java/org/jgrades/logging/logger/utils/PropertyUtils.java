f2bf5ee31cf6bf47346ea4bc5aa75b61fc9e7328
Piotr <jgrades.project@gmail.com>
2015-08-12 22:03:29 +0200

    Added getting .properties file from external filesystem location Edited jg-all pom<- added mvn properties

--- jg-logging/src/main/java/org/jgrades/logging/logger/utils/PropertyUtils.java
+++ jg-logging/src/main/java/org/jgrades/logging/logger/utils/PropertyUtils.java
@@ -5,58 +5,67 @@ import org.apache.commons.lang3.EnumUtils;
 import org.apache.commons.lang3.Validate;
 import org.jgrades.logging.logger.configuration.LoggingConfiguration;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 
 public class PropertyUtils {
 
     private static final String PROPERTIES_FILE = "jg-logging.properties";
+    private static final String CURRENT_LOGGING_PROPERTIES_DIR = "logging.properties.dir";
     private static final String CURRENT_CONFIGURATION_PROPERTY_FILE = "logging.configuration.strategy";
     private static final String CURRENT_LOGGING_LEVEL = "logging.level";
-    private static Properties prop;
+    private static Properties internalProperties,externalProperties;
 
     public synchronized static LoggingConfiguration getCurrentLoggerConfiguration() {
-        checkReadedPropertyFile();
-        return LoggingConfiguration.valueOf(prop.getProperty(CURRENT_CONFIGURATION_PROPERTY_FILE));
+        checkReadPropertyFile();
+        return LoggingConfiguration.valueOf(externalProperties.getProperty(CURRENT_CONFIGURATION_PROPERTY_FILE));
     }
 
     public synchronized static Level getCurrentLoggingLevel(){
-        checkReadedPropertyFile();
-        return Level.valueOf(prop.getProperty(CURRENT_LOGGING_LEVEL));
+        checkReadPropertyFile();
+        return Level.valueOf(externalProperties.getProperty(CURRENT_LOGGING_LEVEL));
     }
 
+
     public static void setNewLoggingLevel(Level level){
-        checkReadedPropertyFile();
-        prop.setProperty(CURRENT_LOGGING_LEVEL, level.toString());
+        checkReadPropertyFile();
+        externalProperties.setProperty(CURRENT_LOGGING_LEVEL, level.toString());
     }
 
     public static void setNewLoggerConfiguration(String newConfiguration) {
-        checkReadedPropertyFile();
+        checkReadPropertyFile();
         Validate.validState(EnumUtils.isValidEnum(LoggingConfiguration.class, newConfiguration));
 
-        prop.setProperty(CURRENT_CONFIGURATION_PROPERTY_FILE, newConfiguration);
+        externalProperties.setProperty(CURRENT_CONFIGURATION_PROPERTY_FILE, newConfiguration);
 
 
     }
 
-    private static void checkReadedPropertyFile(){
-        if(prop == null ) {
-            readPropertyFile();
+    private static void checkReadPropertyFile(){
+        try {
+            if (internalProperties == null) {
+                internalProperties = new Properties();
+                readPropertyFile(internalProperties, Thread.currentThread().getContextClassLoader().getResourceAsStream(PROPERTIES_FILE));
+            }
+            if (externalProperties == null) {
+                externalProperties = new Properties();
+                readPropertyFile(externalProperties, new FileInputStream(new File(internalProperties.getProperty(CURRENT_LOGGING_PROPERTIES_DIR))));
+            }
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
         }
     }
-    private static void readPropertyFile() {
-        prop = new Properties();
+
+    private static void readPropertyFile(Properties propertiesFile,InputStream inputToPropertiesFile) {
         String propFileName = PROPERTIES_FILE;
-        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(propFileName);
 
         try {
-            if (inputStream != null) {
-                prop.load(inputStream);
+            if (inputToPropertiesFile != null) {
+                propertiesFile.load(inputToPropertiesFile);
             } else {
-                throw new FileNotFoundException("Property file '" + propFileName + "' not found in the classpath");
+                throw new FileNotFoundException("Property file '" + propFileName + "' was  not found");
             }
         }
         catch(IOException ex) {
@@ -64,7 +73,7 @@ public class PropertyUtils {
         }
         finally {
             try {
-                inputStream.close();
+                inputToPropertiesFile.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
