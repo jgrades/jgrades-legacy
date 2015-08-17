0c4d0fba9494bc254316eeb6add8a637d788ddd7
baart92 <baart92@gmail.com>
2015-08-17 16:10:21 +0200

    logger improvement

--- jg-logging/src/main/java/org/jgrades/logging/JgLoggerFactory.java
+++ jg-logging/src/main/java/org/jgrades/logging/JgLoggerFactory.java
@@ -3,12 +3,10 @@ package org.jgrades.logging;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
-import org.apache.commons.configuration.Configuration;
 import org.jgrades.logging.dao.LoggingConfigurationDaoFileImpl;
 import org.jgrades.logging.job.ExternalConfigurationMonitoringJob;
 import org.jgrades.logging.job.XmlConfigurationUpdater;
 import org.jgrades.logging.utils.LoggerInternalProperties;
-import org.omg.PortableServer.LIFESPAN_POLICY_ID;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.LoggerFactory;
@@ -52,12 +50,10 @@ public final class JgLoggerFactory {
             Scheduler sched = schedFact.getScheduler();
             sched.start();
 
-            // define the job and tie it to our HelloJob class
             JobDetail job = newJob(ExternalConfigurationMonitoringJob.class)
                     .withIdentity("extConfMonitorJob", "jg-logging")
                     .build();
 
-            // Trigger the job to run now, and then every 40 seconds
             Trigger trigger = newTrigger()
                     .withIdentity("extConfMonitorTrigger", "jg-logging")
                     .startNow()
@@ -66,7 +62,6 @@ public final class JgLoggerFactory {
                             .repeatForever())
                     .build();
 
-            // Tell quartz to schedule the job using our trigger
             sched.scheduleJob(job, trigger);
         } catch (SchedulerException e) {
             e.printStackTrace();
