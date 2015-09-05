b98088eb29a40db687d5361f7ae5e8dd446202b7
baart92 <baart92@gmail.com>
2015-09-05 18:31:39 +0200

    Replace H2 engine with HSQL in licensing database

--- jg-backend/implementation/base/jg-lic/implementation/src/main/java/org/jgrades/lic/config/LicConfig.java
+++ jg-backend/implementation/base/jg-lic/implementation/src/main/java/org/jgrades/lic/config/LicConfig.java
@@ -5,9 +5,7 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
-import org.h2.tools.Server;
 import org.jgrades.lic.api.config.LicApiConfig;
-import org.jgrades.lic.utils.DbCloser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -57,13 +55,9 @@ public class LicConfig {
 
     @Bean
     DataSource licDataSource() throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers").start();
-
-
         HikariConfig dataSourceConfig = new HikariConfig();
-        dataSourceConfig.setDriverClassName("org.h2.Driver");
-        dataSourceConfig.setJdbcUrl("jdbc:h2:tcp://localhost/" + licDbPath);
+        dataSourceConfig.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSourceConfig.setJdbcUrl("jdbc:hsqldb:file:" + licDbPath);
         dataSourceConfig.setUsername("sa");
         dataSourceConfig.setPassword("");
         return new HikariDataSource(dataSourceConfig);
@@ -77,7 +71,7 @@ public class LicConfig {
         entityManagerFactoryBean.setPackagesToScan("org.jgrades.lic.entities");
 
         Properties jpaProperties = new Properties();
-        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
         jpaProperties.put("hibernate.hbm2ddl.auto", schemaOrmPolicy);
         jpaProperties.put("hibernate.show_sql", showSql);
         jpaProperties.put("hibernate.format_sql", formatSql);
@@ -97,10 +91,5 @@ public class LicConfig {
     Mapper mapper() {
         List<String> mappingFiles = Lists.newArrayList("lic_models_mapping.xml");
         return new DozerBeanMapper(mappingFiles);
-    }
-
-    @Bean
-    DbCloser dbCloser(DataSource licDataSource) {
-        return new DbCloser(licDataSource);
     }
 }
